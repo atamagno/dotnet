8937ddbb5d8b9b21a79b82ff2a88720a0ef030be
Nano Taboada <nanotaboada@msn.com>
2012-11-24 03:20:42 -0300

    Adding NBuilder sample project.

---
+++ Dotnet.Samples.NBuilder/Dotnet.Samples.NBuilder/src/BookBuilderTests.cs
@@ -0,0 +1,138 @@
+﻿﻿#region License
+// Copyright (c) 2012 Nano Taboada, http://openid.nanotaboada.com.ar 
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE. 
+#endregion
+
+namespace Dotnet.Samples.NBuilder
+{
+    using System;
+    using System.Linq;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class BookBuilderTests
+    {
+
+        [Test]
+        [TestCase(-1)]
+        public void GetFakes_QuantityLessThanZero_ShouldThrowArgumentException(int quantity)
+        {
+            // Arrange & Act
+            Action act = () => BooksBuilder.GetFakes(quantity);
+
+            // Assert
+            act.ShouldThrow<ArgumentOutOfRangeException>();
+        }
+
+        [Test]
+        [TestCase(8)]
+        public void GetFakes_Isbns_ShouldNotContainNullsAndOnlyHaveUniqueItems(int quantity)
+        {
+            // Arrange & Act
+            var isbns = BooksBuilder.GetFakes(quantity).Select(book => book.Isbn);
+
+            // Assert
+            isbns.Should().NotContainNulls().And.OnlyHaveUniqueItems();
+        }
+
+        [Test]
+        [TestCase(8)]
+        public void GetFakes_Titles_ShouldNotContainNullsAndOnlyHaveUniqueItems(int quantity)
+        {
+            // Arrange & Act
+            var titles = BooksBuilder.GetFakes(quantity).Select(book => book.Title);
+
+            // Assert
+            titles.Should().NotContainNulls().And.OnlyHaveUniqueItems();
+        }
+
+        [Test]
+        [TestCase(8)]
+        public void GetFakes_Authors_ShouldNotContainNullsAndOnlyHaveUniqueItems(int quantity)
+        {
+            // Arrange & Act
+            var authors = BooksBuilder.GetFakes(quantity).Select(book => book.Author);
+
+            // Assert
+            authors.Should().NotContainNulls().And.OnlyHaveUniqueItems();
+        }
+
+
+        [Test]
+        [TestCase(8)]
+        public void GetFakes_Publishers_ShouldNotContainNullsAndOnlyHaveUniqueItems(int quantity)
+        {
+            // Arrange & Act
+            var publishers = BooksBuilder.GetFakes(quantity).Select(book => book.Publisher);
+
+            // Assert
+            publishers.Should().NotContainNulls().And.OnlyHaveUniqueItems();
+        }
+
+        [Test]
+        [TestCase(8)]
+        public void GetFakes_Publications_ShouldNotContainNullsAndBeBetweenStartAndToday(int quantity)
+        {
+            // Arrange
+            var start = 1.January(1900);
+
+            // Act
+            var books = BooksBuilder.GetFakes(quantity);
+
+            // Assert
+            books.Should().NotContainNulls();
+            books.All(book => book.Publication > start && book.Publication < DateTime.Today);
+        }
+
+        [Test]
+        [TestCase(8)]
+        public void GetFakes_Pages_ShouldBeOfLenghtLessThanAThousand(int quantity)
+        {
+            // Arrange & Act
+            var pages = BooksBuilder.GetFakes(quantity).Select(book => book.Pages);
+
+            // Assert
+            pages.All(length => length < 1000);
+        }
+
+        [Test]
+        [TestCase(8)]
+        public void GetFakes_Descriptions_ShouldNotContainNullsAndOnlyHaveUniqueItems(int quantity)
+        {
+            // Arrange & Act
+            var descriptions = BooksBuilder.GetFakes(quantity).Select(book => book.Description);
+
+            // Assert
+            descriptions.Should().NotContainNulls().And.OnlyHaveUniqueItems();
+        }
+
+        [Test]
+        [TestCase(8)]
+        public void GetFakes_InStockFlags_ShouldNotContainNullsAndOnlyContainTrueOrFalse(int quantity)
+        {
+            // Arrange & Act
+            var instockFlags = BooksBuilder.GetFakes(quantity).Select(book => book.InStock);
+
+            // Assert
+            instockFlags.Should().NotContainNulls().And.OnlyContain(value => value == true || value == false);
+        }
+    }
+}
---
+++ Dotnet.Samples.NBuilder/Dotnet.Samples.NBuilder/src/BooksBuilder.cs
@@ -0,0 +1,70 @@
+﻿﻿#region License
+// Copyright (c) 2012 Nano Taboada, http://openid.nanotaboada.com.ar 
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE. 
+#endregion
+
+namespace Dotnet.Samples.NBuilder
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using FizzWare.NBuilder;
+
+    public static class BooksBuilder
+    {
+        private static Random random = new Random();
+
+        public static List<Book> GetFakes(int quantity)
+        {
+            if (quantity < 1) throw new ArgumentOutOfRangeException("Quantity should be positive.");
+
+            return Builder<Book>
+                .CreateListOfSize(quantity)
+                    .All()
+                        .With(book => book.Isbn = GetRandomFakeIsbn())
+                        .With(book => book.Publication = GetRandomPastDate())
+                        .With(book => book.Pages = random.Next(0,1000))
+                        .With(book => book.InStock = Convert.ToBoolean(random.Next(0,2)))
+                .Build()
+                    .ToList();
+        }
+
+        private static string GetRandomFakeIsbn()
+        {
+            var ean = "978";
+            var group = random.Next(0, 2).ToString("0");
+            var publisher = random.Next(200, 699).ToString("000");
+            var title = random.Next(0, 1000000).ToString("000000");
+            var check = random.Next(0, 10).ToString("0"); // Not a real checksum!
+
+            return string.Format("{0}-{1}-{2}-{3}-{4}", ean, group, publisher, title, check);
+        }
+
+        private static DateTime GetRandomPastDate()
+        {
+            var start = new DateTime(1900,1,1);
+            var range = ((TimeSpan)(DateTime.Today - start)).Days;
+
+            return start.AddDays(random.Next(range));
+        }
+    }
+
+
+}
