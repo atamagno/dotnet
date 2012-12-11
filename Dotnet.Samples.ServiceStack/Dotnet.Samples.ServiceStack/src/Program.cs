b8cb616c64b16cc6a82a41fce98cfcae6015734a
Nano Taboada <nanotaboada@msn.com>
2012-12-11 13:03:23 -0300

    Adding ServiceStack sample project.

---
+++ Dotnet.Samples.ServiceStack/Dotnet.Samples.ServiceStack/src/Program.cs
@@ -0,0 +1,58 @@
+﻿#region License
+// Copyright (c) 2012 Nano Taboada, http://openid.nanotaboada.com.ar 
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE. 
+#endregion
+
+using System;
+
+namespace Dotnet.Samples.ServiceStack
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            try 
+            {
+                var uri = new Uri("http://127.0.0.1:8080/");
+
+                var host = new Host();
+                host.Init();
+
+                host.Start(uri.AbsoluteUri);
+                Console.WriteLine(string.Format("[RUNNING] ServiceStack Self-Hosted Service at {0}", uri.AbsoluteUri));
+                Console.ReadKey(true);
+            
+                host.Stop();
+                Console.WriteLine(string.Format("[STOPPED] ServiceStack Self-Hosted Service at {0}", uri.AbsoluteUri));
+            }
+            catch (Exception exception)
+            {
+                Console.Write(Environment.NewLine);
+                Console.WriteLine(exception.ToString());
+            }
+            finally
+            {
+                Console.Write(Environment.NewLine);
+                Console.Write("Press any key to continue . . .");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
