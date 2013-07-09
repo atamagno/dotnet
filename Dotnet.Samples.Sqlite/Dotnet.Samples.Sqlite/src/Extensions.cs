0f8707b419e0690caf45653f1741f5a3afca5bde
Nano Taboada <nanotaboada@msn.com>
2013-07-09 18:33:49 -0300

    Refactored helpers into extensions. Suppressed StyleCop SA1600.

--- Dotnet.Samples.Sqlite/Dotnet.Samples.Sqlite/src/Extensions.cs
+++ Dotnet.Samples.Sqlite/Dotnet.Samples.Sqlite/src/Extensions.cs
@@ -1,44 +1,81 @@
-﻿#region License
-// Copyright (c) 2011 Nano Taboada, http://openid.nanotaboada.com.ar 
+﻿// -----------------------------------------------------------------------------
+// <copyright file="Extensions.cs" company="NanoTaboada">
+//   Copyright (c) 2013 Nano Taboada, http://openid.nanotaboada.com.ar 
 // 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
+//   Permission is hereby granted, free of charge, to any person obtaining a copy
+//   of this software and associated documentation files (the "Software"), to deal
+//   in the Software without restriction, including without limitation the rights
+//   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//   copies of the Software, and to permit persons to whom the Software is
+//   furnished to do so, subject to the following conditions:
 // 
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
+//   The above copyright notice and this permission notice shall be included in
+//   all copies or substantial portions of the Software.
 // 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE. 
-#endregion
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//   THE SOFTWARE.
+// </copyright>
+// -----------------------------------------------------------------------﻿------
+
+[module: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "For educational purposes only.")]
 
 namespace Dotnet.Samples.Sqlite
 {
-    #region References
+    using System;
+    using System.Data.SQLite;
+    using System.IO;
+    using System.Reflection;
     using System.Text;
-    #endregion
 
     public static class Extensions
     {
+        public static string BuildConnectionString(this SQLiteConnectionStringBuilder builder)
+        {
+            var directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var path = Path.Combine(Path.Combine(directory, "res"), "Catalog.sqlite");
+            builder.DataSource = path;
+
+            return builder.ConnectionString;
+        }
+
+        public static string FormatValues(this SQLiteDataReader reader)
+        {
+            var builder = new StringBuilder();
+
+            builder.AppendLine(string.Format("{0,-37} {1,-23} {2,10} {3,5}", "-".Repeat(37), "-".Repeat(23), "-".Repeat(10), "-".Repeat(5)));
+            builder.AppendLine(string.Format("{0,-37} {1,-23} {2,-10} {3,-5}", "Title", "Author", "Published", "Pages"));
+            builder.AppendLine(string.Format("{0,-37} {1,-23} {2,10} {3,5}", "-".Repeat(37), "-".Repeat(23), "-".Repeat(10), "-".Repeat(5)));
+
+            while (reader.Read())
+            {
+                builder.AppendFormat("{0,-37} {1,-23} {2,10} {3,5}", reader.GetString(1), reader.GetString(2), reader.GetDateTime(4).ToShortDateString(), reader.GetValue(5));
+                builder.Append(Environment.NewLine);
+            }
+
+            builder.AppendLine(string.Format("{0,-37} {1,-23} {2,10} {3,5}", "-".Repeat(37), "-".Repeat(23), "-".Repeat(10), "-".Repeat(5)));
+
+            return builder.ToString();
+        }
+
         public static string Repeat(this char character, int frequency)
         {
             return new string(character, frequency);
         }
+
         public static string Repeat(this string content, int frequency)
         {
             var container = new StringBuilder(frequency * content.Length);
+
             for (int i = 0; i < frequency; i++)
             {
                 container.Append(content);
             }
+
             return container.ToString();
         }
     }
--- Dotnet.Samples.Sqlite/Dotnet.Samples.Sqlite/src/Program.cs
+++ Dotnet.Samples.Sqlite/Dotnet.Samples.Sqlite/src/Program.cs
@@ -1,43 +1,47 @@
-﻿﻿#region License
-// Copyright (c) 2012 Nano Taboada, http://openid.nanotaboada.com.ar 
+﻿// -----------------------------------------------------------------------------
+// <copyright file="Program.cs" company="NanoTaboada">
+//   Copyright (c) 2013 Nano Taboada, http://openid.nanotaboada.com.ar 
 // 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
+//   Permission is hereby granted, free of charge, to any person obtaining a copy
+//   of this software and associated documentation files (the "Software"), to deal
+//   in the Software without restriction, including without limitation the rights
+//   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//   copies of the Software, and to permit persons to whom the Software is
+//   furnished to do so, subject to the following conditions:
 // 
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
+//   The above copyright notice and this permission notice shall be included in
+//   all copies or substantial portions of the Software.
 // 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE. 
-#endregion
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//   THE SOFTWARE.
+// </copyright>
+// -----------------------------------------------------------------------﻿------
 
-#region References
-using System;
-using System.Data.SQLite;
-#endregion
+[module: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "For educational purposes only.")]
 
 namespace Dotnet.Samples.Sqlite
 {
-    class Program
+    using System;
+    using System.Data.SQLite;
+
+    public class Program
     {
-        static void Main()
+        public static void Main()
         {
+            var connectionString = new SQLiteConnectionStringBuilder().BuildConnectionString();
             var statement = "SELECT * FROM Books";
 
             try
             {
-                using (var connection = new SQLiteConnection(Helpers.GetConnectionString()))
+                using (var connection = new SQLiteConnection(connectionString))
                 {
                     connection.Open();
+
                     using (var transaction = connection.BeginTransaction())
                     {
                         try
@@ -48,30 +52,31 @@ namespace Dotnet.Samples.Sqlite
                                 {
                                     if (reader.HasRows)
                                     {
-                                        Console.WriteLine(Helpers.FormatConsoleOutput(reader));   
+                                        Console.WriteLine(reader.FormatValues());   
                                     }
                                 }
                             }
+
                             transaction.Commit();
                         }
                         catch (Exception)
                         {
                             transaction.Rollback();
-                            throw;
                         }
                     }
+
                     connection.Close();
                 }
             }
             catch (Exception error)
             {
                 Console.Write(Environment.NewLine);
-                Console.WriteLine(String.Format("Exception: {0}", error.ToString()));
+                Console.WriteLine(string.Format("Exception: {0}", error.ToString()));
             }
             finally
             {
                 Console.Write(Environment.NewLine);
-                Console.Write("Press any key to continue . . .");
+                Console.WriteLine("Press any key to continue . . .");
                 Console.ReadKey(true);
             }
         }
