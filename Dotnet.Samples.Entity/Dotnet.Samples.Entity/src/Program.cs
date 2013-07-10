9945a995693596be0cb6767242abc177313ed652
Nano Taboada <nanotaboada@msn.com>
2013-07-10 01:37:30 -0300

    Refactored helpers into extensions. Suppressed StyleCop SA1600.

--- Dotnet.Samples.Entity/Dotnet.Samples.Entity/src/Program.cs
+++ Dotnet.Samples.Entity/Dotnet.Samples.Entity/src/Program.cs
@@ -1,35 +1,37 @@
-﻿#region License
-// Copyright (c) 2012 Nano Taboada, http://openid.nanotaboada.com.ar 
+﻿// -----------------------------------------------------------------------------
+// <copyright file="Program.cs" company="NanoTaboada">
+//   Copyright (c) 2013 Nano Taboada, http://openid.nanotaboada.com.ar 
 // 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
+//   Permission is hereby granted, free of charge, to any person obtaining a copy
+//   of this software and associated documentation files (the "Software"), to deal
+//   in the Software without restriction, including without limitation the rights
+//   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//   copies of the Software, and to permit persons to whom the Software is
+//   furnished to do so, subject to the following conditions:
 // 
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
+//   The above copyright notice and this permission notice shall be included in
+//   all copies or substantial portions of the Software.
 // 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE. 
-#endregion
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//   THE SOFTWARE.
+// </copyright>
+// -----------------------------------------------------------------------﻿------
 
-#region References
-using System;
-using System.Linq;
-#endregion
+[module: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "For educational purposes only.")]
 
 namespace Dotnet.Samples.Entity
 {
-    class Program
+    using System;
+    using System.Linq;
+
+    public class Program
     {
-        static void Main()
+        public static void Main()
         {
             try
             {
@@ -40,18 +42,23 @@ namespace Dotnet.Samples.Entity
                                 orderby book.Published
                                 select book;
 
-                    Console.WriteLine(Helpers.FormatConsoleOutput(books));
+                    Console.WriteLine(books); // outputs generated SQL
+                    Console.Write(Environment.NewLine);
+                    Console.WriteLine("Press any key to continue . . .");
+                    Console.ReadKey(true);
+                    Console.Clear();
+                    Console.WriteLine(books.FormatValues());
                 }
             }
             catch (Exception error)
             {
                 Console.Write(Environment.NewLine);
-                Console.WriteLine(String.Format("Exception: {0}", error.ToString()));
+                Console.WriteLine(string.Format("Exception: {0}", error.ToString()));
             }
             finally
             {
                 Console.Write(Environment.NewLine);
-                Console.Write("Press any key to continue . . .");
+                Console.WriteLine("Press any key to continue . . .");
                 Console.ReadKey(true);
             }
         }
