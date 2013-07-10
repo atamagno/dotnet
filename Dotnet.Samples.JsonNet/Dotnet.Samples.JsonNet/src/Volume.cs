0c452854870a9aa606b4a6dfc15a7ea1bae00757
Nano Taboada <nanotaboada@msn.com>
2013-07-10 02:34:21 -0300

    Suppressed StyleCop SA1402, SA1600. Renamed Stub volume.

--- Dotnet.Samples.JsonNet/Dotnet.Samples.JsonNet/src/Volume.cs
+++ Dotnet.Samples.JsonNet/Dotnet.Samples.JsonNet/src/Volume.cs
@@ -1,18 +1,29 @@
-﻿#region License
-// Copyright 2011 Google Corporation
+﻿// -----------------------------------------------------------------------------
+// <copyright file="Volume.cs" company="NanoTaboada">
+//   Copyright (c) 2013 Nano Taboada, http://openid.nanotaboada.com.ar 
 // 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+//   Permission is hereby granted, free of charge, to any person obtaining a copy
+//   of this software and associated documentation files (the "Software"), to deal
+//   in the Software without restriction, including without limitation the rights
+//   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//   copies of the Software, and to permit persons to whom the Software is
+//   furnished to do so, subject to the following conditions:
 // 
-//     http://www.apache.org/licenses/LICENSE-2.0
+//   The above copyright notice and this permission notice shall be included in
+//   all copies or substantial portions of the Software.
 // 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-#endregion
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//   THE SOFTWARE.
+// </copyright>
+// -----------------------------------------------------------------------﻿------
+
+[module: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "For educational purposes only.")]
+[module: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "For educational purposes only.")]
 
 namespace Dotnet.Samples.JsonNet
 {
@@ -100,21 +111,20 @@ namespace Dotnet.Samples.JsonNet
 
     public class DownloadAccessRestriction
     {
-
         [JsonPropertyAttribute("deviceAllowed")]
-        public virtual Nullable<bool> DeviceAllowed { get; set; }
+        public virtual bool? DeviceAllowed { get; set; }
 
         [JsonPropertyAttribute("downloadsAcquired")]
-        public virtual Nullable<long> DownloadsAcquired { get; set; }
+        public virtual long? DownloadsAcquired { get; set; }
 
         [JsonPropertyAttribute("justAcquired")]
-        public virtual Nullable<bool> JustAcquired { get; set; }
+        public virtual bool? JustAcquired { get; set; }
 
         [JsonPropertyAttribute("kind")]
         public virtual string Kind { get; set; }
 
         [JsonPropertyAttribute("maxDownloadDevices")]
-        public virtual Nullable<long> MaxDownloadDevices { get; set; }
+        public virtual long? MaxDownloadDevices { get; set; }
 
         [JsonPropertyAttribute("message")]
         public virtual string Message { get; set; }
@@ -126,7 +136,7 @@ namespace Dotnet.Samples.JsonNet
         public virtual string ReasonCode { get; set; }
 
         [JsonPropertyAttribute("restricted")]
-        public virtual Nullable<bool> Restricted { get; set; }
+        public virtual bool? Restricted { get; set; }
 
         [JsonPropertyAttribute("signature")]
         public virtual string Signature { get; set; }
--- Dotnet.Samples.JsonNet/Dotnet.Samples.JsonNet/src/VolumeTests.cs
+++ Dotnet.Samples.JsonNet/Dotnet.Samples.JsonNet/src/VolumeTests.cs
@@ -1,39 +1,42 @@
-﻿﻿#region License
-// Copyright (c) 2012 Nano Taboada, http://openid.nanotaboada.com.ar 
+﻿// -----------------------------------------------------------------------------
+// <copyright file="VolumeTests.cs" company="NanoTaboada">
+//   Copyright (c) 2013 Nano Taboada, http://openid.nanotaboada.com.ar 
 // 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
+//   Permission is hereby granted, free of charge, to any person obtaining a copy
+//   of this software and associated documentation files (the "Software"), to deal
+//   in the Software without restriction, including without limitation the rights
+//   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//   copies of the Software, and to permit persons to whom the Software is
+//   furnished to do so, subject to the following conditions:
 // 
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
+//   The above copyright notice and this permission notice shall be included in
+//   all copies or substantial portions of the Software.
 // 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE. 
-#endregion
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//   THE SOFTWARE.
+// </copyright>
+// -----------------------------------------------------------------------﻿------
+
+[module: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "For educational purposes only.")]
 
 namespace Dotnet.Samples.JsonNet
 {
     using Newtonsoft.Json;
-    using Newtonsoft.Json.Linq;
     using NUnit.Framework;
 
     [TestFixture]
     public class VolumeTests
     {
         [Test]
-        public void SerializeObject_WhenJsonStringIsDeserializedToTypeThenSerialized_StringsAreEqual()
+        public void SerializeObject_WhenExpectedValueIsDeserializedAndActualValueIsSerialized_ThenAreEqual()
         {
             // Arrange
-            var expected = VolumeFactory.GetFakeVolume();
+            var expected = VolumeStub.Create();
             var volume = JsonConvert.DeserializeObject<Volume>(expected);
 
             // Act
