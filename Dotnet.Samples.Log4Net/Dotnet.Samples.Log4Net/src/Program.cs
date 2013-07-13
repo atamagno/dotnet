8c1839b6cf9e3e9b03677aa96714ea497351bf6a
Nano Taboada <nanotaboada@msn.com>
2013-07-13 16:10:41 -0300

    Suppressed StyleCop SA1600.

--- Dotnet.Samples.Log4Net/Dotnet.Samples.Log4Net/src/Program.cs
+++ Dotnet.Samples.Log4Net/Dotnet.Samples.Log4Net/src/Program.cs
@@ -1,61 +1,83 @@
-﻿#region License
-// Copyright (c) 2010 Nano Taboada, http://openid.nanotaboada.com.ar 
+﻿// ----------------------------------------------------------------------------
+// <copyright file="Program.cs" company="NanoTaboada">
+//   Copyright (c) 2013 Nano Taboada, http://openid.nanotaboada.com.ar 
 // 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
+//   Permission is hereby granted, free of charge, to any person obtaining a copy
+//   of this software and associated documentation files (the "Software"), to deal
+//   in the Software without restriction, including without limitation the rights
+//   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//   copies of the Software, and to permit persons to whom the Software is
+//   furnished to do so, subject to the following conditions:
 // 
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
+//   The above copyright notice and this permission notice shall be included in
+//   all copies or substantial portions of the Software.
 // 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE. 
-#endregion
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//   THE SOFTWARE.
+// </copyright>
+// -----------------------------------------------------------------------﻿-----
+
+[module: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "For educational purposes only.")]
 
 namespace Dotnet.Samples.Log4Net
 {
-    #region References
     using System;
     using System.IO;
     using System.Reflection;
     using System.Xml;
     using log4net;
     using log4net.Config;
-    #endregion
 
-    class Program
+    public class Program
     {
-        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Program));
 
-        static void Main()
+        public static void Main()
         {
             try
             {
-                var dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-                var cfg = Path.Combine(dir, "cfg");
+                var directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                var path = Path.Combine(directory, "cfg");
+                var message = "The quick brown fox jumps over the lazy dog.";
+
                 var xml = new XmlDocument();
-                    xml.Load(Path.Combine(cfg, "Log4Net.config"));
-                var msg = "The quick brown fox jumps over the lazy dog.";
+                xml.Load(Path.Combine(path, "Log4Net.config"));
                 XmlConfigurator.Configure((XmlElement)xml.DocumentElement);
 
-                if (log.IsDebugEnabled) log.Debug(msg);
-                if (log.IsInfoEnabled) log.Info(msg);
-                if (log.IsWarnEnabled) log.Warn(msg);
-                if (log.IsErrorEnabled) log.Error(msg);
-                if (log.IsFatalEnabled) log.Fatal(msg);
+                if (Log.IsDebugEnabled)
+                {
+                    Log.Debug(message);
+                }
+
+                if (Log.IsInfoEnabled)
+                {
+                    Log.Info(message);
+                }
+
+                if (Log.IsWarnEnabled)
+                {
+                    Log.Warn(message);
+                }
+
+                if (Log.IsErrorEnabled)
+                {
+                    Log.Error(message);
+                }
+
+                if (Log.IsFatalEnabled)
+                {
+                    Log.Fatal(message);
+                }
             }
-            catch (Exception err)
+            catch (Exception exception)
             {
                 Console.Write(Environment.NewLine);
-                Console.WriteLine(String.Format("Exception: {0}", err.Message));
+                Console.WriteLine(string.Format("Exception: {0}", exception.ToString()));
             }
             finally
             {
