6e27702166463428b59b2267d5ee41ad6cc37635
Nano Taboada <nanotaboada@msn.com>
2013-07-21 01:58:25 -0300

    Adding MSTest sample project.

---
+++ Dotnet.Samples.MSTest/Dotnet.Samples.MSTest/Program.cs
@@ -0,0 +1,53 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="Program.cs" company="NanoTaboada">
+//   Copyright (c) 2013 Nano Taboada, http://openid.nanotaboada.com.ar 
+// 
+//   Permission is hereby granted, free of charge, to any person obtaining a copy
+//   of this software and associated documentation files (the "Software"), to deal
+//   in the Software without restriction, including without limitation the rights
+//   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//   copies of the Software, and to permit persons to whom the Software is
+//   furnished to do so, subject to the following conditions:
+// 
+//   The above copyright notice and this permission notice shall be included in
+//   all copies or substantial portions of the Software.
+// 
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//   THE SOFTWARE.
+// </copyright>
+// -----------------------------------------------------------------------﻿-----
+
+[module: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "For educational purposes only.")]
+[module: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "Reviewed.")]
+
+namespace Dotnet.Samples.MSTest
+{
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        /// <summary>
+        /// A pangram (Greek: pan gramma, "every letter"), or holoalphabetic 
+        /// sentence, is a sentence using every letter of the alphabet at least
+        /// once. Pangrams have been used to display typefaces, test equipment,
+        /// and develop skills in handwriting, calligraphy, and keyboarding.
+        /// </summary>
+        /// <returns>A dictionary containing pangrams and its language codes.</returns>
+        public Dictionary<string, string> GetPangrams()
+        {
+            var pangrams = new Dictionary<string, string>();
+
+            pangrams.Add("DE", "Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich.");
+            pangrams.Add("EN", "The quick brown fox jumps over the lazy dog.");
+            pangrams.Add("ES", "El veloz murciélago hindú comía feliz cardillo y kiwi. La cigüeña tocaba el saxofón detrás del palenque de paja.");
+            pangrams.Add("FR", "Portez ce vieux whisky au juge blond qui fume.");
+
+            return pangrams;
+        }
+    }
+}
